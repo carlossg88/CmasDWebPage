68edd92815cf8224747aee1e6dad4a826a03832a
carlossg88 <carlossg88@hotmail.com>
2023-03-09 19:18:45 -0500

    Adding about and functionalities on JS

--- main.js
+++ main.js
@@ -1,9 +1,17 @@
-const menuHamIcon = document.querySelector('.menuIcon')
-const MobileMenu = document.querySelector('.navbar-right-mobile')
+const menuHamIcon = document.querySelector('.menuIcon');
+const MobileMenu = document.querySelector('.navbar-right-mobile');
+const BackgroundImage = document.querySelector('.BackgroundImage');
 
-menuHamIcon.addEventListener('click',toggleMobileMenu)
+menuHamIcon.addEventListener('click',toggleMobileMenu);
 
+contador = 0;
 function toggleMobileMenu(){
-  MobileMenu.classList.toggle('inactive')
-  console.log('click aqui')
+  
+  MobileMenu.classList.toggle('inactive');
+  BackgroundImage.setAttribute('src', './img'+contador+'.jpg');
+  contador = contador +1;
+  console.log(contador);
+  if (contador == 5){
+    contador=0
+  }
 }
