a5f8c0db8fed86402c8be09500beef977b9226ef
carlossg88 <carlossg88@hotmail.com>
2023-04-04 08:24:14 -0500

    Adding mouseover and mouseout to projects page

--- main.js
+++ main.js
@@ -2,9 +2,13 @@ const menuHamIcon = document.querySelector('.menuIcon');
 const MobileMenu = document.querySelector('.navbar-right-mobile');
 const BackgroundImage = document.querySelector('.BackgroundImage');
 
+
 menuHamIcon.addEventListener('click',toggleMobileMenu);
+
 
 
 function toggleMobileMenu(){
   MobileMenu.classList.toggle('inactive');
 }
+
+
