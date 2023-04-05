644a797e9db019e1e2a22e1066c7f1399c908328
carlossg88 <carlossg88@hotmail.com>
2023-04-05 08:28:03 -0500

    Adding floating images to show project images

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
+// functions for menu icon
 const menuHamIcon = document.querySelector('.menuIcon');
 const MobileMenu = document.querySelector('.navbar-right-mobile');
-const BackgroundImage = document.querySelector('.BackgroundImage');
-
 
 menuHamIcon.addEventListener('click',toggleMobileMenu);
 
-
-
 function toggleMobileMenu(){
   MobileMenu.classList.toggle('inactive');
+}
+
+
+// function for close floating image
+const menuClose = document.querySelector('.close-Icon');
+const imgRoll = document.querySelector('.ImgRoll');
+menuClose.addEventListener('click',CloseImage);
+function CloseImage(){
+  imgRoll.classList.add('inactive')
 }
 
 
